47c2b66868e4def4881fbd15f651222f04c272e3
Кирилл Абрамов <58049946+abkvl@users.noreply.github.com>
2020-11-05 14:39:14 +0300

    Update remove_palindroms.py

--- remove_palindroms.py
+++ remove_palindroms.py
@@ -1,9 +1,1 @@
-def remove_palindroms(spells):
-    result = []
-    for word in spells:
-        temp = word.lower().strip()
-        if temp != temp[::-1]:
-            result.append(word)
-
-    return result
-
+0
